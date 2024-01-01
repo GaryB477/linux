e2f1a29ce48580cc15eeca03966a5434fb3cf819
Marc Röthlisberger <m.roethlisberger477@gmail.com>
2024-01-01 15:18:45 +0100

    Added raid instruction and automount to nixos config

--- nixos/configuration_nas.nix
+++ nixos/configuration_nas.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
     ];
 
   # Bootloader.
@@ -15,6 +15,15 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
+
+  # The raid was created with mdadm using this command:
+  # sudo mdadm --create /dev/md0 --level=1 --raid-devices=2 /dev/sda /dev/sdb
+  # Note: check the sdX values to ensure the correct deviecs are selected when creating!
+  fileSystems."/home/marc/raid" =
+    { device = "/dev/md0";
+      fsType = "ext4";
+    };
+
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -35,20 +44,20 @@
   console.keyMap = "de";
 
 
-# Add docker support
-virtualisation.docker.enable = true;
-
-
+  # Add docker support
+  virtualisation.docker.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.marc = {
     isNormalUser = true;
     description = "Marc Röthlisberger";
-    extraGroups = [ "networkmanager" "wheel" "docker" "storage" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
 
   packages = with pkgs; [
   vim 
   git
+  mdadm
+  gparted
   ];
   };
 
@@ -60,6 +69,7 @@ virtualisation.docker.enable = true;
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.openssh.settings.X11Forwarding= true;
 
   system.stateVersion = "23.11"; # Did you read the comment?
 }
