a361c6471f10a9285011309ce95a6a28dde8e1cd
Marc Röthlisberger <marc.roethlisberger@digitecgalaxus.ch>
2025-04-02 11:30:47 +0200

    Cleanup and introduced nix-alien

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,23 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgsunstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
+    nix-alien.url = "github:thiagokokada/nix-alien";
+
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgsunstable,
-    nvf,
-    ...
-  } @ inputs: {
+  outputs = { self, nixpkgs, nvf, ... }@inputs: {
     nixosConfigurations = {
       work_dg = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
-          nvf.nixosModules.default
           ./configurations/configuration_work_dg.nix
+          nvf.nixosModules.default
         ];
-        #specialArgs = nixpkgsunstable;
       };
     };
   };
