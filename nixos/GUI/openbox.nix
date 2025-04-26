d6aa84b3f21d85fc5be0da38e7ea594be4419252
Marc Röthlisberger <m.roethlisberger477@gmail.com>
2025-04-26 17:16:22 +0200

    Fixed mismatched window managers for openbox config

--- nixos/GUI/openbox.nix
+++ nixos/GUI/openbox.nix
@@ -12,12 +12,6 @@
     windowManager.openbox.enable = true;
   };
 
-  # services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
   users.users.marc = {
     packages = with pkgs; [
       obconf
