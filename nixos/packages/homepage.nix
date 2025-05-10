087cdb26cd7ac32ef9dbfde5a908d982866f6ae5
Marc Röthlisberger <m.roethlisberger477@gmail.com>
2025-05-10 16:09:05 +0200

    Added homepage service setup skeleton

---
+++ nixos/packages/homepage.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+{
+  services.homepage-dashboard = {
+    # These options were already present in my configuration.
+    enable = true;
+    # package = unstable.homepage-dashboard;
+
+    # The following options were what I planned to add.
+
+    # https://gethomepage.dev/latest/configs/settings/
+    settings = {};
+
+    # https://gethomepage.dev/latest/configs/bookmarks/
+    bookmarks = [];
+
+    # https://gethomepage.dev/latest/configs/services/
+    services = [];
+
+    # https://gethomepage.dev/latest/configs/service-widgets/
+    widgets = [];
+
+    # https://gethomepage.dev/latest/configs/kubernetes/
+    kubernetes = { };
+
+    # https://gethomepage.dev/latest/configs/docker/
+    docker = { };
+
+    # https://gethomepage.dev/latest/configs/custom-css-js/
+    customJS = "";
+    customCSS = "";
+  };
+}
