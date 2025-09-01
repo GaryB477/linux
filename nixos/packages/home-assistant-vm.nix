e7767c1974ef9a8f75c66f8c9046ebfd054f468e
Marc Röthlisberger <m.roethlisberger477@gmail.com>
2025-09-01 11:34:04 +0200

    Configure bridge network for Home Assistant VM

--- nixos/packages/home-assistant-vm.nix
+++ nixos/packages/home-assistant-vm.nix
@@ -22,13 +22,34 @@
   users.users.marc= {
     extraGroups = ["libvirtd"];
   };
- # networking.defaultGateway.address = "10.0.0.1";
+
+  # Bridge network configuration for Home Assistant VM
   networking.bridges.br0.interfaces = ["enp4s0"];
-  networking.interfaces.br0 = {
-    #useDHCP = false;
-    ipv4.addresses = [{
-      "address" = "192.168.0.50";
-      "prefixLength" = 24;
-    }];
+  networking.interfaces = {
+    # Disable DHCP on the physical interface since it's now part of the bridge
+    enp4s0.useDHCP = false;
+    
+    # Configure the bridge interface
+    br0 = {
+      useDHCP = false;
+      ipv4.addresses = [{
+        address = "192.168.0.50";
+        prefixLength = 24;
+      }
+      {
+        address = "192.168.0.34";
+        prefixLength = 24;
+      } 
+      ];
+    };
   };
+  
+  # Set default gateway and DNS servers
+  networking.defaultGateway = {
+    address = "192.168.0.254";
+    interface = "br0";
+  };
+  
+  # Configure DNS servers (using common public DNS servers as fallback)
+  networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
 }
